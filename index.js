52ec6febc28a3864395c68384cc441ed328e0960
mariluzv88 <59103912+mariluzv88@users.noreply.github.com>
2023-08-08 22:13:14 -0400

    Magic 8 Ball express application created.

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express = require("express")
 
 const app = express()
+const random = require('./models/responses')
 
-app.get('/greeting/:name',(req,res)=>{
-  res.send(`Wow! Hello There,${req.params.name}`)
-})
-app.get("/tip/:total/:tipPercentage",(req,res)=>{
-  res.send(`The tip is ${req.params.tipPercentage}`)
+
+ 
+  app.get('/greeting/:name',(req,res)=>{
+      res.send(`Wow! Hello There,${req.params.name}`)
+    })
+    app.get("/tip/:total/:tipPercentage",(req,res)=>{
+        res.send(`The tip is ${req.params.tipPercentage}`)
+    })
+    // let random = response[Math.floor(Math.random()*response.length)]
+app.get('/magic/:question',(req,res)=>{
+    res.send(`${req.params.question},<h1>${random}</h1>`)
+    
+    // res.render("Response",{response:random})
+    // res.send(random)
+    // ,${random}
 })
 
 app.listen('3000',(req,res)=>{
---
+++ models/responses.js
@@ -0,0 +1,12 @@
+const response = ["It is certain", "It is decidedly so", 
+"Without a doubt", "Yes definitely","You may rely on it",
+ "As I see it yes", "Most likely", "Outlook good","Yes",
+  "Signs point to yes", "Reply hazy try again", 
+  "Ask again later","Better not tell you now", 
+  "Cannot predict now", "Concentrate and ask again",
+  "Don't count on it", "My reply is no", "My sources say no"
+  ,"Outlook not so good", "Very doubtful"]
+
+  const random = response[Math.floor(Math.random()*response.length)]
+  module.exports = random
+//   export default random
