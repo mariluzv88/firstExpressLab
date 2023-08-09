5b63233d6637f3574a01c3559faa1e0e5db78790
mariluzv88 <59103912+mariluzv88@users.noreply.github.com>
2023-08-08 20:37:53 -0400

    Tip Calculator express application created.

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const app = express()
 app.get('/greeting/:name',(req,res)=>{
   res.send(`Wow! Hello There,${req.params.name}`)
 })
-
+app.get("/tip/:total/:tipPercentage",(req,res)=>{
+  res.send(`The tip is ${req.params.tipPercentage}`)
+})
 
 app.listen('3000',(req,res)=>{
     console.log("Port on 3K")
